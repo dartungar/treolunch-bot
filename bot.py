d0b15d57afecac21c55e409c35051914fe269e86
dartungar <dartungar@gmail.com>
2021-01-08 14:46:10 +0300

    formula tweaks & some decomposition

--- bot.py
+++ bot.py
@@ -11,20 +11,24 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
-place_chooser = choose_place.PlaceChooser()
-
-keyboard = ReplyKeyboardMarkup([['/start']], True)
-
 
 # init bot
 # nothing special, just 1 handler for 1 command we have
 def main():
 
+    # init helper classes
+    weather = choose_place.Weather(
+        coordinates={'lat': 55.769505, 'lon': 37.672348}, api_key=os.getenv('API_KEY'))
+    place_chooser = choose_place.PlaceChooser()
+
+    keyboard = ReplyKeyboardMarkup([['/start']], True)
+
     updater = Updater(token=BOT_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
     def get_place(update, context):
-        place = place_chooser.get_full_info()
+        place = place_chooser.get_full_info(
+            weather.current_weather, weather._get_weather_rating())
         context.bot.send_message(
             chat_id=update.effective_chat.id, text=place, reply_markup=keyboard)
 
@@ -34,8 +38,5 @@ def main():
     updater.start_polling()
 
 
-# PULL IT UP CAPTAIN
-# NRRRRNG
-# TO THE MOON!
 if __name__ == '__main__':
     main()
--- choose_place.py
+++ choose_place.py
@@ -3,47 +3,62 @@ import os
 import random
 import requests
 from helpers import get_declinated_minute_word
+from datetime import datetime, timedelta
 
 
+class Weather:
+    '''encapsulates weather-related logic. \n
+        getting actual weather from API & getting it's custom-defined "score" (./weather.json)'''
+
+    def __init__(self, coordinates, api_key):
+        self._api_key = api_key
+        self._coordinates = coordinates
+        self._last_request_time = None
+        self._last_request_weather = None
+        with open("weather.json", encoding='utf-8') as file:
+            self.weather_data = json.load(file)
+
+    @property
+    def current_weather(self):
+        '''Get actual weather data, if last request was > 15m ago. Else return current data'''
+        if not self._last_request_time or not self._last_request_weather or self._last_request_time < datetime.now() - timedelta(minutes=15):
+            self._last_request_time = datetime.now()
+            self._last_request_weather = requests.get(
+                f'https://api.openweathermap.org/data/2.5/weather?lat={self._coordinates["lat"]}&lon={self._coordinates["lon"]}&units=metric&lang=ru&appid={self._api_key}').json()
+        return self._last_request_weather
+
+    def _get_weather_rating(self) -> int:
+        '''  get 'goodness' rating of the weather based on its openweathermap code.\n
+            'clear' weather is 1, 'heavy thunderstorm' is 0.01, etc.\n
+            data is in ./weather.json'''
+        rating = [w for w in self.weather_data if w['code']
+                  == str(self._last_request_weather['weather'][0]['id'])][0]['rating']
+        return rating
+
+
+# encapculated place-choosing operations
+# 'given this weather, what do you recommend?'
 class PlaceChooser:
     # load places & weather info, env variables
     def __init__(self) -> None:
         with open("places.json", encoding='utf-8') as file:
             self.places = json.load(file)
-        with open("weather.json", encoding='utf-8') as file:
-            self.weather = json.load(file)
 
-        try:
-            self.api_key = os.getenv('API_KEY')
-        except:
-            raise EnvironmentError('API_KEY env variable not found!')
-
-    # get weather data for given coordinates from openweathermap API
-    # default coords: Treolan office on Dobro5
-    def _get_current_weather(self, location={'lat': 55.769505, 'lon': 37.672348}):
-        lat, lon = location['lat'], location['lon']
-        resp = requests.get(
-            f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&lang=ru&appid={self.api_key}')
-        if resp.status_code != 200:
-            raise Exception(
-                f'Request was not successful: code {resp.status_code}')
-        return resp.json()
-
-    # get 'goodness' rating of the weather based on its openweathermap code
-    # 'clear' weather is 1, 'heavy thunderstorm' is 0.01, etc.
-    # see ./weather.json
-    def _get_weather_rating(self, code):
-        rating = [w for w in self.weather if w['code']
-                  == str(code)][0]['rating']
-        return rating
-
-    # the better the weather is, the farther places we can/want to go
-    # this function simulates that
-    def _choose_place(self, temp, weather_rating):
+    def _choose_place(self, temp: int, weather_rating: int) -> dict:
+        '''Get place based on current weather and distance to the place. \n
+            The worse weather's rating, the less distance we are 'willing to walk'.\n
+            Extreme temperature takes its toll, too.'''
         max_distance = 1500 * weather_rating
         # if it's too warm or too cold, we probably wouldn't want to go too far
-        if temp > 26 or temp < -10:
-            max_distance /= 2
+        tempdelta = 0
+        if temp > 24:
+            tempdelta = temp - 24
+        elif temp < -3:
+            tempdelta = abs(temp - -3)
+        tempdelta = abs(tempdelta**2 * 10)
+        max_distance -= tempdelta
+        if max_distance < 100:
+            max_distance = 100
         suitable_places = [
             p for p in self.places if p['distance'] <= max_distance]
         place_obj = random.choice(suitable_places)
@@ -54,24 +69,26 @@ class PlaceChooser:
 
         return {'name': place_name, 'distance': place_obj['distance']}
 
-    # choose place based on weather data
-    def get_random_place(self, weather):
+    def get_random_place(self, weather: dict, weather_rating: int) -> dict:
+        '''choose place based on weather data'''
         try:
             place = self._choose_place(
-                weather['main']['feels_like'], self._get_weather_rating(weather['weather'][0]['id']))
+                weather['main']['feels_like'], weather_rating)
             return place
         except Exception as e:
             raise e
 
-    # get info & create full response string
-    def get_full_info(self):
-        weather = self._get_current_weather()
-        place = self.get_random_place(weather)
+    def get_full_info(self, weather: dict, weather_rating: int) -> str:
+        '''get info & create full response string'''
+        place = self.get_random_place(weather, weather_rating)
+        temp = weather['main']['feels_like']
         walking_minutes = round(place['distance'] / 80)
-        return f"Сейчас на улице {weather['weather'][0]['description']}. \nТемпература по ощущениям: {weather['main']['feels_like']}С. \nПредлагаю сходить в {place['name']}. \nИдти примерно {walking_minutes} {get_declinated_minute_word(walking_minutes)}."
+        return f"Сейчас на улице {weather['weather'][0]['description']}.\nТемпература по ощущениям: {temp}С.\nПредлагаю сходить в {place['name']}. \nИдти примерно {walking_minutes} {get_declinated_minute_word(walking_minutes)}."
 
 
 # for testing
 if __name__ == '__main__':
+    weather = Weather(
+        coordinates={'lat': 55.769505, 'lon': 37.672348}, api_key=os.getenv('API_KEY'))
     bot = PlaceChooser()
-    print(bot.get_full_info())
+    print(bot.get_full_info(weather.current_weather, weather._get_weather_rating()))
